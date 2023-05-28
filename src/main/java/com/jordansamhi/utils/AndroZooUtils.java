9a4da4dfacf6cb6385ee76ab0a67fde552458c5c
Jordan Samhi <j.samhi@me.com>
2023-05-28 08:10:20 +0200

    Add: copyright

--- src/main/java/com/jordansamhi/utils/AndroZooUtils.java
+++ src/main/java/com/jordansamhi/utils/AndroZooUtils.java
@@ -9,6 +9,31 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 
+/*-
+ * #%L
+ * Utils
+ *
+ * %%
+ * Copyright (C) 2023 Jordan Samhi
+ * All rights reserved
+ *
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 /**
  * Utility class for downloading APK files from AndroZoo using the provided API key and path.
  */
--- src/main/java/com/jordansamhi/utils/FlowdroidUtils.java
+++ src/main/java/com/jordansamhi/utils/FlowdroidUtils.java
@@ -8,6 +8,31 @@ import soot.jimple.infoflow.android.SetupApplication;
 import soot.jimple.infoflow.android.manifest.ProcessManifest;
 import soot.jimple.infoflow.config.IInfoflowConfig;
 
+/*-
+ * #%L
+ * Utils
+ *
+ * %%
+ * Copyright (C) 2023 Jordan Samhi
+ * All rights reserved
+ *
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
 /**
  * A utility class for initializing and running Flowdroid analyses on Android apps.
  */
