e020ad807aa0a3dd4015758d52fa3134dc9fb7db
Jordan Samhi <j.samhi@me.com>
2023-07-30 16:59:04 +0200

    Add: run taint analysis

--- src/main/java/com/jordansamhi/androspecter/FlowdroidUtils.java
+++ src/main/java/com/jordansamhi/androspecter/FlowdroidUtils.java
@@ -1,12 +1,22 @@
 package com.jordansamhi.androspecter;
 
 import com.jordansamhi.androspecter.config.SootConfig;
+import com.jordansamhi.androspecter.printers.Writer;
 import soot.G;
+import soot.SootMethod;
+import soot.jimple.Stmt;
 import soot.jimple.infoflow.InfoflowConfiguration;
 import soot.jimple.infoflow.android.InfoflowAndroidConfiguration;
 import soot.jimple.infoflow.android.SetupApplication;
+import soot.jimple.infoflow.android.data.AndroidMethod;
 import soot.jimple.infoflow.android.manifest.ProcessManifest;
 import soot.jimple.infoflow.config.IInfoflowConfig;
+import soot.jimple.infoflow.results.InfoflowResults;
+import soot.jimple.infoflow.results.ResultSinkInfo;
+import soot.jimple.infoflow.results.ResultSourceInfo;
+import soot.jimple.infoflow.solver.cfg.InfoflowCFG;
+
+import java.util.*;
 
 /*-
  * #%L
@@ -36,15 +46,17 @@ import soot.jimple.infoflow.config.IInfoflowConfig;
 /**
  * A utility class for initializing and running Flowdroid analyses on Android apps.
  *
- * @author Jordan Samhi
+ * @author <a href="https://jordansamhi.com">Jordan Samhi</a>
  */
 public class FlowdroidUtils {
 
     private ProcessManifest pm;
     private final String apkPath;
+    private SetupApplication sa;
 
     public FlowdroidUtils(String apkPath) {
         this.apkPath = apkPath;
+        this.sa = null;
         try {
             pm = new ProcessManifest(this.apkPath);
         } catch (Exception e) {
@@ -91,7 +103,66 @@ public class FlowdroidUtils {
         if (useExistingInstance) {
             sa.getConfig().setSootIntegrationMode(InfoflowAndroidConfiguration.SootIntegrationMode.UseExistingInstance);
         }
+        this.sa = sa;
         return sa;
+    }
+
+    /**
+     * Runs the taint analysis on the provided set of sources and sinks.
+     *
+     * The method throws a NullPointerException if the SetupApplication has not been initialized before calling this method.
+     * It then runs the Infoflow analysis and stores the results in a set of strings where each string represents a leak path
+     * from a source to a sink in the format:
+     * "Found leak:
+     *   - From [source]
+     *     - Detailed path:
+     *        [statement] => in method: [method]
+     *   - To [sink]"
+     *
+     * If no flow is found, a warning message is printed.
+     *
+     * @param sources the set of sources to consider in the taint analysis
+     * @param sinks   the set of sinks to consider in the taint analysis
+     * @return a set of strings, each string describing a path from a source to a sink
+     * @throws NullPointerException if the SetupApplication has not been initialized
+     */
+    public Set<String> runTaintAnalysis(Set<AndroidMethod> sources, Set<AndroidMethod> sinks) {
+        if (this.sa == null) {
+            throw new NullPointerException("The SetupApplication has not been initialized.\n" +
+                    "First call the initializeFlowdroid method.");
+        }
+        InfoflowResults results = null;
+        try {
+            results = sa.runInfoflow(sources, sinks);
+        } catch (Exception e) {
+
+        }
+        InfoflowCFG icfg = new InfoflowCFG();
+        List<SootMethod> sourceList = null;
+        Set<String> resultList = new HashSet<>();
+        if (results != null) {
+            if (results.getResults() != null && !results.getResults().isEmpty()) {
+                for (ResultSinkInfo sink : results.getResults().keySet()) {
+                    for (ResultSourceInfo source : results.getResults().get(sink)) {
+                        List<Stmt> path = Arrays.asList(source.getPath());
+                        if (path != null && !path.isEmpty()) {
+                            StringBuilder resultBuilder = new StringBuilder();
+                            resultBuilder.append("Found leak: \n");
+                            resultBuilder.append("  - From ").append(source).append("\n");
+                            resultBuilder.append("    - Detailed path:\n");
+                            for (Stmt s : path) {
+                                resultBuilder.append("       ").append(s).append(" => in method: ").append(icfg.getMethodOf(s)).append("\n");
+                            }
+                            resultBuilder.append("  - To ").append(sink).append("\n");
+                            resultList.add(resultBuilder.toString());
+                        }
+                    }
+                }
+            } else {
+                Writer.v().pwarning("No Flow found.");
+            }
+        }
+        return resultList;
     }
 
     /**
