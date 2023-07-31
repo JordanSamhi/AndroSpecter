7378b02e1bb4a9a313c20ed466cad8c46f8bff1c
Jordan Samhi <j.samhi@me.com>
2023-07-31 10:39:49 +0200

    Add: command line options

---
+++ src/main/java/com/jordansamhi/androspecter/commonlineoptions/CommandLineOption.java
@@ -0,0 +1,191 @@
+package com.jordansamhi.androspecter.commonlineoptions;
+
+/*-
+ * #%L
+ * AndroSpecter
+ *
+ * %%
+ * Copyright (C) 2023 Jordan Samhi
+ * All rights reserved
+ *
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+/**
+ * Represents a command line option.
+ * Encapsulates all the properties of a command line option like its long and short form,
+ * description, whether it requires an argument, whether it's mandatory, and its value.
+ *
+ * @author <a href="https://jordansamhi.com">Jordan Samhi</a>
+ */
+public class CommandLineOption {
+
+    /**
+     * The long form of the option. E.g., "help"
+     */
+    private String longOpt;
+
+    /**
+     * The short form of the option. E.g., "h"
+     */
+    private String shortOpt;
+
+    /**
+     * A brief description of what the option does.
+     */
+    private String description;
+
+    /**
+     * Flag to indicate if the option requires an argument.
+     */
+    private boolean hasArg;
+
+    /**
+     * Flag to indicate if the option is mandatory.
+     */
+    private boolean isRequired;
+
+    /**
+     * The value of the option if provided on the command line.
+     */
+    private String value;
+
+    /**
+     * Constructor for CommandLineOption.
+     *
+     * @param longOpt     The long form of the option.
+     * @param shortOpt    The short form of the option.
+     * @param description The description of the option.
+     * @param hasArg      True if the option requires an argument.
+     * @param isRequired  True if the option is mandatory.
+     */
+    public CommandLineOption(String longOpt, String shortOpt, String description, boolean hasArg, boolean isRequired) {
+        this.longOpt = longOpt;
+        this.shortOpt = shortOpt;
+        this.description = description;
+        this.hasArg = hasArg;
+        this.isRequired = isRequired;
+    }
+
+    /**
+     * Returns the long form of the option.
+     *
+     * @return The long form of the option.
+     */
+    public String getLongOpt() {
+        return longOpt;
+    }
+
+    /**
+     * Sets the long form of the option.
+     *
+     * @param longOpt The long form of the option.
+     */
+    public void setLongOpt(String longOpt) {
+        this.longOpt = longOpt;
+    }
+
+    /**
+     * Returns the short form of the option.
+     *
+     * @return The short form of the option.
+     */
+    public String getShortOpt() {
+        return shortOpt;
+    }
+
+    /**
+     * Sets the short form of the option.
+     *
+     * @param shortOpt The short form of the option.
+     */
+    public void setShortOpt(String shortOpt) {
+        this.shortOpt = shortOpt;
+    }
+
+    /**
+     * Returns the description of the option.
+     *
+     * @return The description of the option.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Sets the description of the option.
+     *
+     * @param description The description of the option.
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * Returns whether the option requires an argument.
+     *
+     * @return True if the option requires an argument, false otherwise.
+     */
+    public boolean hasArg() {
+        return hasArg;
+    }
+
+    /**
+     * Sets whether the option requires an argument.
+     *
+     * @param hasArg True if the option requires an argument, false otherwise.
+     */
+    public void setHasArg(boolean hasArg) {
+        this.hasArg = hasArg;
+    }
+
+    /**
+     * Returns whether the option is mandatory.
+     *
+     * @return True if the option is mandatory, false otherwise.
+     */
+    public boolean isRequired() {
+        return isRequired;
+    }
+
+    /**
+     * Sets whether the option is mandatory.
+     *
+     * @param required True if the option is mandatory, false otherwise.
+     */
+    public void setRequired(boolean required) {
+        isRequired = required;
+    }
+
+    /**
+     * Returns the value of the option.
+     *
+     * @return The value of the option.
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the option.
+     *
+     * @param value The value of the option.
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ src/main/java/com/jordansamhi/androspecter/commonlineoptions/CommandLineOptions.java
@@ -0,0 +1,187 @@
+package com.jordansamhi.androspecter.commonlineoptions;
+
+import com.jordansamhi.androspecter.printers.Writer;
+import org.apache.commons.cli.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*-
+ * #%L
+ * AndroSpecter
+ *
+ * %%
+ * Copyright (C) 2023 Jordan Samhi
+ * All rights reserved
+ *
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 2.1 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ * #L%
+ */
+
+
+/**
+ * A singleton class to handle command line options parsing and validation.
+ * Encapsulates Apache Commons CLI library for parsing command line options.
+ *
+ * @author <a href="https://jordansamhi.com">Jordan Samhi</a>
+ */
+public class CommandLineOptions {
+
+    /**
+     * The Apache Commons CLI Options object for regular options.
+     */
+    private final Options options;
+
+    /**
+     * The Apache Commons CLI Options object for first options (like help).
+     */
+    private final Options firstOptions;
+
+    /**
+     * The Apache Commons CLI Parser object to parse command line options.
+     */
+    private final CommandLineParser parser;
+
+    /**
+     * The Apache Commons CLI CommandLine object, holds parsed command line options.
+     */
+    private CommandLine cmdLine;
+
+    /**
+     * The application name.
+     */
+    private String appName;
+
+    /**
+     * Singleton instance of this class.
+     */
+    private static CommandLineOptions instance;
+
+    /**
+     * List of CommandLineOption objects.
+     */
+    private final List<CommandLineOption> optionList;
+
+    /**
+     * Constructor for CommandLineOptions.
+     */
+    private CommandLineOptions() {
+        this.options = new Options();
+        this.firstOptions = new Options();
+        this.parser = new DefaultParser();
+        this.optionList = new ArrayList<>();
+        final Option help = Option.builder("h")
+                .longOpt("help")
+                .desc("Prints this message")
+                .argName("h")
+                .build();
+        this.firstOptions.addOption(help);
+        this.appName = null;
+    }
+
+    /**
+     * Returns the singleton instance of CommandLineOptions.
+     *
+     * @return The singleton instance of CommandLineOptions.
+     */
+    public static CommandLineOptions v() {
+        if (instance == null) {
+            instance = new CommandLineOptions();
+        }
+        return instance;
+    }
+
+    /**
+     * Parses command line arguments.
+     *
+     * @param args The command line arguments.
+     */
+    public void parseArgs(String[] args) {
+        if (this.appName == null) {
+            Writer.v().perror("First sets the name of your program, use the setAppName method.");
+            System.exit(1);
+        }
+        HelpFormatter formatter;
+        try {
+            CommandLine cmdFirstLine = this.parser.parse(this.firstOptions, args, true);
+            if (cmdFirstLine.hasOption("help")) {
+                formatter = new HelpFormatter();
+                formatter.printHelp(this.getAppName(), this.options, true);
+                System.exit(0);
+            }
+            cmdLine = parser.parse(options, args);
+
+            for (CommandLineOption opt : optionList) {
+                if (cmdLine.hasOption(opt.getShortOpt())) {
+                    opt.setValue(cmdLine.getOptionValue(opt.getShortOpt()));
+                }
+            }
+        } catch (ParseException e) {
+            Writer.v().perror(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    /**
+     * Adds a CommandLineOption to the list of options.
+     *
+     * @param option The CommandLineOption to add.
+     */
+    public void addOption(CommandLineOption option) {
+        optionList.add(option);
+        Option cliOption = Option.builder(option.getShortOpt())
+                .longOpt(option.getLongOpt())
+                .desc(option.getDescription())
+                .hasArg(option.hasArg())
+                .argName(option.getLongOpt())
+                .required(option.isRequired())
+                .build();
+        options.addOption(cliOption);
+    }
+
+    /**
+     * Returns the value of the specified option.
+     *
+     * @param longOpt The long form of the option.
+     * @return The value of the specified option, or null if not found.
+     */
+    public String getOptionValue(String longOpt) {
+        for (CommandLineOption opt : optionList) {
+            if (opt.getLongOpt().equals(longOpt)) {
+                return opt.getValue();
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Returns the application name.
+     *
+     * @return The application name.
+     */
+    public String getAppName() {
+        return appName;
+    }
+
+    /**
+     * Sets the application name.
+     *
+     * @param appName The application name.
+     */
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+}
